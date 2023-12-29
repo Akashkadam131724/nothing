fbf42cf1694b50ea48fa77326f30d53e53c0b849
Akashkadam131724 <kadamakash131724@gmail.com>
2023-12-29 14:57:52 +0530

    29-dec

---
+++ vanilla-js/01-29-dec/29-dec.js
@@ -0,0 +1,12 @@
+// palindrome number problem
+const isPalindrome = (inputNum) => {
+  return inputNum === +inputNum.toString().split("").reverse().join("");
+};
+
+console.log(isPalindrome(12));
+
+// fibonacci series
+
+// valid anagram
+
+//Two sum
